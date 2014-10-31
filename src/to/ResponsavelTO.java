affcaf76630f1a7ec48e900504780e7e6234d13d
Maicon <gouveia.maicon@gmail.com>
2014-10-31 02:03:12 -0200

    Progresso 30% no Checkin

--- src/to/ResponsavelTO.java
+++ src/to/ResponsavelTO.java
@@ -2,117 +2,59 @@ package to;
 
 public class ResponsavelTO {
 	
-	private int    idCompra;
+	private int id;
+	private String nome;
+	private String telefone;
 	private String email;
-	private String celular;
-	private String bandeira;
-	private String nomeTitular;
-	private String cpf;
-	private String numCartao;
-	private String dataValidade;
-	private String codigoSeguranca;
-	private String banco;
-	private String agencia;
-	private String conta;
 	
-	public ResponsavelTO( String email, String celular,
-			String bandeira, String nomeTitular, String cpf, String numCartao,
-			String dataValidade, String codigoSeguranca) {
-		this.email = email;
-		this.celular = celular;
-		this.bandeira = bandeira;
-		this.nomeTitular = nomeTitular;
-		this.cpf = cpf;
-		this.numCartao = numCartao;
-		this.dataValidade = dataValidade;
-		this.codigoSeguranca = codigoSeguranca;
+	public ResponsavelTO(){
 		
 	}
-	
-	public ResponsavelTO( String email, String celular,
-			 String nomeTitular, String cpf, String banco,
-			String agencia, String conta) {
+	public ResponsavelTO(int id, String nome, String telefone, String email){
+		this.id = id;
+		this.nome = nome;
+		this.telefone = telefone;
 		this.email = email;
-		this.celular = celular;
-		this.nomeTitular = nomeTitular;
-		this.cpf = cpf;
-		this.banco = banco;
-		this.agencia = agencia;
-		this.conta = conta;
 	}
-	
-	public int getIdCompra() {
-		return idCompra;
+
+	public int getId() {
+		return id;
 	}
-	public void setIdCompra(int idCompra) {
-		this.idCompra = idCompra;
+
+
+	public void setId(int id) {
+		this.id = id;
 	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+
 	public String getEmail() {
 		return email;
 	}
+
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getCelular() {
-		return celular;
-	}
-	public void setCelular(String celular) {
-		this.celular = celular;
-	}
-	public String getBandeira() {
-		return bandeira;
-	}
-	public void setBandeira(String bandeira) {
-		this.bandeira = bandeira;
-	}
-	public String getNomeTitular() {
-		return nomeTitular;
-	}
-	public void setNomeTitular(String nomeTitular) {
-		this.nomeTitular = nomeTitular;
-	}
-	public String getCpf() {
-		return cpf;
-	}
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
-	public String getNumCartao() {
-		return numCartao;
-	}
-	public void setNumCartao(String numCartao) {
-		this.numCartao = numCartao;
-	}
-	public String getDataValidade() {
-		return dataValidade;
-	}
-	public void setDataValidade(String dataValidade) {
-		this.dataValidade = dataValidade;
-	}
-	public String getCodigoSeguranca() {
-		return codigoSeguranca;
-	}
-	public void setCodigoSeguranca(String codigoSeguranca) {
-		this.codigoSeguranca = codigoSeguranca;
-	}
-	public String getBanco() {
-		return banco;
-	}
-	public void setBanco(String banco) {
-		this.banco = banco;
-	}
-	public String getAgencia() {
-		return agencia;
-	}
-	public void setAgencia(String agencia) {
-		this.agencia = agencia;
-	}
-	public String getConta() {
-		return conta;
-	}
-	public void setConta(String conta) {
-		this.conta = conta;
-	}
-	
+
 	
 }
